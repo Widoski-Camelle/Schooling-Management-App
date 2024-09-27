5e61238329d41716b801e5fe59170df8a4fb279a
Widoski-Camelle <camelle-widoski.houndagba@epitech.eu>
2024-09-27 12:19:31 +0100

    Title and button create 'Nouvelle Année Scolaire' implemented

--- Schooling-Management-App/resources/views/livewire/settings.blade.php
+++ Schooling-Management-App/resources/views/livewire/settings.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Because she competes with no one, no one can compete with her. --}}
+<div class="mt-5">
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2">
+        {{-- Titre et bouton créer --}}
+
+        <div class="flex justify-between items-center">
+            <h4>Liste des années scolaires</h4>
+            <a href="#" class="bg-blue-500 rounded-md p-2 text-sm text-white">Nouvelle Année Scolaire</a>
+        </div>
+    </div>
 </div>
