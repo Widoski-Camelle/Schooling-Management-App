2d581054343972aae2394ed4890233a0837d3115
Widoski-Camelle <camelle-widoski.houndagba@epitech.eu>
2024-09-27 12:58:14 +0100

    Table added in settings component for listing school years

--- Schooling-Management-App/resources/views/livewire/settings.blade.php
+++ Schooling-Management-App/resources/views/livewire/settings.blade.php
@@ -1,6 +1,6 @@
 <div class="mt-5">
-    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2">
-        {{-- Titre et bouton créer --}}
+    <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
+        <!-- Titre et bouton créer -->
 
         <div class="flex justify-between items-center">
             <h4>Liste des années scolaires</h4>
@@ -8,10 +8,47 @@
         </div>
 
         <div class="flex flex-col">
-            {{-- Message qui apparaîtra après opération --}}
+            <!-- Message qui apparaîtra après opération -->
 
             <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. A, tempore.
+            </div>
+        </div>
+
+        <!-- Styliser le tableau -->
+
+        <div class="overflow-x-auto">
+            <div class="py-4 inline-block min-w-full">
+                <div class="overflow-hidden">
+                    <table class="min-w-full text-center">
+
+                        <thead class="border-b bg-gray-50">
+                            <tr>
+                                <th class="text-sm font-medium text-gray-900 px-6 py-6">Libellé</th>
+                                <th class="text-sm font-medium text-gray-900 px-6 py-6">Libellé</th>
+                                <th class="text-sm font-medium text-gray-900 px-6 py-6">Libellé</th>
+                                <th class="text-sm font-medium text-gray-900 px-6 py-6">Libellé</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr class="border-b-2 border-gray-100">
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                            </tr>
+                            <tr class="border-b-2 border-gray-100">
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                                <td class="text-sm font-medium text-gray-900 px-6 py-6">Libellé Exemple</td>
+                            </tr>
+                        </tbody>
+
+                    </table>
+
+                    <!-- Pagination -->
+                </div>
             </div>
         </div>
     </div>
--- Schooling-Management-App/resources/views/settings/index.blade.php
+++ Schooling-Management-App/resources/views/settings/index.blade.php
@@ -5,11 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                <livewire:settings />
-            </div>
-        </div>
+    <div class="py-2 px-12">
+        <livewire:settings />
     </div>
 </x-app-layout>
