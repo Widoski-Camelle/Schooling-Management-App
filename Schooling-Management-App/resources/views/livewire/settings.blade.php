6810cbaa99d9060eb4c1af6922491850e29bfbd0
Widoski-Camelle <camelle-widoski.houndagba@epitech.eu>
2024-09-27 12:33:26 +0100

    Alert message added

--- Schooling-Management-App/resources/views/livewire/settings.blade.php
+++ Schooling-Management-App/resources/views/livewire/settings.blade.php
@@ -6,5 +6,13 @@
             <h4>Liste des années scolaires</h4>
             <a href="#" class="bg-blue-500 rounded-md p-2 text-sm text-white">Nouvelle Année Scolaire</a>
         </div>
+
+        <div class="flex flex-col">
+            {{-- Message qui apparaîtra après opération --}}
+
+            <div class="block p-2 bg-green-500 text-white rounded-sm shadow-sm mt-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. A, tempore.
+            </div>
+        </div>
     </div>
 </div>
