1f91234817d1e74a1541c6a9ae5b104f5b73136a
Widoski-Camelle <camelle-widoski.houndagba@epitech.eu>
2024-10-12 15:23:37 +0100

    Level modifying finalized

--- Schooling-Management-App/app/Http/Livewire/EditLevel.php
+++ Schooling-Management-App/app/Http/Livewire/EditLevel.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Level;
 use Livewire\Component;
 
 class EditLevel extends Component
@@ -17,6 +18,32 @@ class EditLevel extends Component
         $this->code = $this->level->code;
         $this->libelle = $this->level->libelle;
         $this->scolarite = $this->level->scolarite;
+    }
+
+    public function store()
+    {
+        $level = Level::find($this->level->id);
+        
+        $this->validate([
+            'code' => 'string|required',
+            'libelle' => 'string|required',
+            'scolarite' => 'integer|required'
+        ]);
+
+        try {
+
+            $level->code = $this->code;
+            $level->libelle = $this->libelle;
+            $level->scolarite = $this->scolarite;
+
+            $level->save();
+
+            return redirect()->route('niveaux')->with('success', 'Niveau mis à jour');
+        } catch (Exception $e)
+        {
+            // Sera pris en compte si on a un problème
+            dd('Error');
+        }
     }
 
     public function render()
